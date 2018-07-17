0e2a55839db18184fff96c1ac17dd5ca70b42348
bdieu178 <bdieu17@gmail.com>
2018-07-17 19:24:53 -0400

    First commit to c++ folder

---
+++ codewars-solns/c++/alt_cases_strings_char.cpp
@@ -0,0 +1,22 @@
+//
+//  Define a function for alternatiing cases : " altERnaTIng cAsE <=> ALTerNAtiNG CaSe"
+//
+//
+
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <cctype>
+using namespace std;
+
+string to_alternating_case(const string& str){ 
+  string *p = new string; 
+  *p = str;
+  //Iterate through each character in the string
+  for (int i = 0; i < (*p).size(); ++i){
+     if( (bool)isupper( (*p)[i] ) )    { (*p)[i] = tolower ( (*p)[i] );  }
+     else if ( (bool)islower((*p)[i])) { (*p)[i] = toupper ( (*p)[i] );  }
+  }
+  cout << (*p) << endl;
+  return *p;
+}
